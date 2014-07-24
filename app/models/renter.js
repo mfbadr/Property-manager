7c50f9a92dc5dce884770ec5467571ef998985f1
Mikey Badr <mfbadr@gmail.com>
2014-07-24 10:24:32 -0500

    added #work

--- app/models/renter.js
+++ app/models/renter.js
@@ -5,9 +5,27 @@ function Renter(name, age, gender, profession){
   this.age = parseInt(age);
   this.gender = gender;
   this.profession = profession;
-  this.cash = Math.floor(Math.random() * (4901) + 100);
+  this.cash = Math.floor((Math.random() * 4901) + 100);
   this.isEvicted = false;
 
 }
   
+
+Renter.prototype.work = function(){
+  switch (this.profession){
+    case 'waiter':
+      this.cash += Math.floor((Math.random() * 201) + 50);
+      break;
+    case 'coder':
+      this.cash += Math.floor((Math.random() * 6001) + 1000);
+      break;
+    case 'movie star':
+      this.cash += Math.floor((Math.random() * 7001) + 3000);
+      break;
+    case 'social worker':
+      this.cash += Math.floor((Math.random() * 601) + 150);
+      break;
+    
+  }
+};
 module.exports = Renter;
--- test/unit/renter.js
+++ test/unit/renter.js
@@ -17,4 +17,12 @@ describe('Renter', function(){
       expect(bob.profession).to.equal('waiter');
     });
   });
+  describe('#work', function(){
+    it('should add to cash', function(){
+      var bob = new Renter('bob', '31', 'male', 'waiter');
+      bob.cash = 0;
+      bob.work();
+      expect(bob.cash).to.be.within(50, 250);
+    });
+  });
 });
